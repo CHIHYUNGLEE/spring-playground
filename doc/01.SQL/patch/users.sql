f22401743b792edd1ad2284c666e6017d941a346
CHIHYUNGLEE <eno125@naver.com>
2025-09-08 15:20:11 +0900

    회원상태 추가

--- doc/01.SQL/patch/users.sql
+++ doc/01.SQL/patch/users.sql
@@ -1,3 +1,9 @@
+--비밀번호 초기화.
 ALTER TABLE users
 ADD COLUMN reset_token VARCHAR(255) DEFAULT NULL,
 ADD COLUMN reset_token_expiry DATETIME DEFAULT NULL;
+
+
+-- 0: 정상
+-- 9100: 탈퇴
+ALTER TABLE users ADD COLUMN status INT DEFAULT 0;
