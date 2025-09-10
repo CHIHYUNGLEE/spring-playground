a520b8ba6e37dd155cca275efc33be677111fe1e
CHIHYUNGLEE <eno125@naver.com>
2025-09-10 17:38:25 +0900

    게시글 댓글용 테이블 추가

--- doc/01.SQL/install/board.sql
+++ doc/01.SQL/install/board.sql
@@ -7,3 +7,20 @@ CREATE TABLE board_posts (
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     FOREIGN KEY (author_id) REFERENCES users(id) ON DELETE CASCADE
 );
+
+-- 댓글
+CREATE TABLE board_comments (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    post_id BIGINT NOT NULL,                 -- 어떤 게시글의 댓글인지
+    parent_id BIGINT NULL,                   -- 부모 댓글 (NULL이면 일반 댓글, 값 있으면 대댓글)  
+    user_id BIGINT NOT NULL,                 -- 작성자
+    content VARCHAR(500) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NULL,
+    CONSTRAINT fk_post FOREIGN KEY (post_id)
+        REFERENCES board_posts(id) ON DELETE CASCADE,
+    CONSTRAINT fk_user FOREIGN KEY (user_id)
+        REFERENCES users(id) ON DELETE CASCADE,
+    CONSTRAINT fk_parent FOREIGN KEY (parent_id)
+        REFERENCES board_comments(id) ON DELETE CASCADE
+);
