962e566be17b9a808be23e876af00b5dcba6c61f
CHIHYUNGLEE <eno125@naver.com>
2025-09-10 13:46:15 +0900

    USER 엔티티 추가 및 권한에 ROLE_ 붙이기

--- src/main/java/com/chihyunglee/springplayground/security/CustomUserDetails.java
+++ src/main/java/com/chihyunglee/springplayground/security/CustomUserDetails.java
@@ -1,21 +1,29 @@
 package com.chihyunglee.springplayground.security;
 
-import java.util.Collection;
+import java.util.Collections;
 
-import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import com.chihyunglee.springplayground.model.User;
 
 public class CustomUserDetails extends org.springframework.security.core.userdetails.User {
     private int status;
+    private User user; // 실제 User 엔티티 추가
 
-    public CustomUserDetails(String username, String password,
-                             Collection<? extends GrantedAuthority> authorities,
-                             int status) {
-        super(username, password, authorities);
-        this.status = status;
+    public CustomUserDetails(User user) {
+        super(user.getUserId(),
+              user.getPassword(),
+              Collections.singleton(new SimpleGrantedAuthority("ROLE_" + user.getRole())));// 권한 처리 (ADMIN / USER)
+        this.user = user;
+        this.status = user.getStatus();
     }
 
     public int getStatus() {
         return status;
+    }
+
+    public User getUser() {
+        return user;
     }
 }
 
