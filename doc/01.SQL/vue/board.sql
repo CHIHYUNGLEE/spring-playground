ee2887a8eb3beab51f389301a88d566ac368a949
CHIHYUNGLEE <eno125@naver.com>
2026-08-10 13:39:28 +0900

    vue용 게시판 sql 추가

---
+++ doc/01.SQL/vue/board.sql
@@ -0,0 +1,12 @@
+-- 20260807 vue 관련 실습 사용 
+-- db : spring_playground, id : myuser pw : 1234
+CREATE TABLE board (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(200) NOT NULL
+);
+SELECT * FROM board;
+update board set title = '공지사항 등록 테스트' where id = 3;
+update board set title = 'Vue 연동 CRUD 확인' where id = 4;
+INSERT INTO board(title)
+VALUES ('Spring Boot API 연동 테스트');
+commit;
